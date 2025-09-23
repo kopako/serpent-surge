8f6d8f533b203a0f08d1234b8fff311a42c87993
kopako <kopandima@gmail.com>
2025-09-23 17:50:56 +0200

    Terraform format

--- terraform/provision/outputs.tf
+++ terraform/provision/outputs.tf
@@ -1,7 +1,7 @@
 output "rds_endpoint" {
-    value = module.rds.rds_hostname
+  value = module.rds.rds_hostname
 }
 
 output "ec2_endpoint" {
-    value = module.ec2_instance.instance_ip
+  value = module.ec2_instance.instance_ip
 }
